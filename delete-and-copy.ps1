b19315374d5dbc177a45e8c0150d75586ccfba23
Henry Post <henryfbp@gmail.com>
2021-09-27 18:15:09 -0500

    fix double path

--- delete-and-copy.ps1
+++ delete-and-copy.ps1
@@ -1,2 +1,2 @@
 Remove-Item -Path 'My Web Sites' -Recurse -Force
-Copy-item -Path "C:\My Web Sites" -Recurse -Destination 'My Web Sites'
+Copy-item -Path "C:\My Web Sites" -Recurse -Destination './'
